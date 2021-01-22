fa7207cdc5b4e69b7195891d2dad240a4befb397
Flávio Henrique <flaviokluppell3.0@outlook.com>
2021-01-22 12:28:29 -0300

    nav prev and next

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-import Slide from './slide.js'
+import {SlideNav, Slide} from './slide.js'
 
-const slide = new Slide('.slide','.slide-wrapper');
+const slide = new SlideNav('.slide','.slide-wrapper');
 slide.init();
+slide.addArrow('.prev','.next');
 
-slide.changeSlide(2);
-slide.activePrevSlide();
