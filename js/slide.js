a0265d2312a92406973fc56f9817911e117b731c
Flávio Henrique <flaviokluppell3.0@outlook.com>
2021-01-24 18:50:33 -0300

    custom controls

--- js/slide.js
+++ js/slide.js
@@ -163,7 +163,7 @@ activeNextSlide() {
 
 
 
-export class SlideNav extends Slide {
+export default class SlideNav extends Slide {
   constructor(slide,wrapper) {
     super(slide,wrapper);
     this.bindControlEvents();
